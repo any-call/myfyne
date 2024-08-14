1fa4741cade9a7dc4828f10b82d34eb39df855d6
jinguihua <156711203@qq.com>
2024-08-14 17:09:39 +0800

    changed

--- base.go
+++ base.go
@@ -1,7 +1,37 @@
 package myfyne
 
+import "fyne.io/fyne/v2"
+
 type (
 	EdgeInset struct {
 		Top, Right, Bottom, Left float32
 	}
 )
+
+func GetWindow(obj fyne.CanvasObject) fyne.Window {
+	listWindow := fyne.CurrentApp().Driver().AllWindows()
+
+	for _, win := range listWindow {
+		if containsObject(win.Content(), obj) {
+			return win
+		}
+	}
+
+	return nil
+}
+
+// 递归查找 obj 是否在 root 中
+func containsObject(root, obj fyne.CanvasObject) bool {
+	if root == obj {
+		return true
+	}
+
+	if container, ok := root.(*fyne.Container); ok {
+		for _, child := range container.Objects {
+			if containsObject(child, obj) {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ base.window.go
@@ -0,0 +1,42 @@
+package myfyne
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
+)
+
+type BaseWindow struct {
+	app    fyne.App
+	window fyne.Window
+}
+
+func NewBaseWindow(app fyne.App) *BaseWindow {
+	return &BaseWindow{
+		app:    app,
+		window: nil,
+	}
+}
+
+func (self *BaseWindow) GetApp() fyne.App {
+	if self.app == nil {
+		self.app = fyne.CurrentApp()
+	}
+	return self.app
+}
+
+func (self *BaseWindow) GetWindow() fyne.Window {
+	if self.window == nil {
+		self.window = self.GetApp().NewWindow("")
+	}
+
+	return self.window
+}
+
+func (self *BaseWindow) Quit() {
+	self.GetApp().Quit()
+}
+
+// 显示对话框提示信息
+func (self *BaseWindow) ShowInfo(message string) {
+	dialog.ShowInformation("Info", message, self.GetWindow())
+}
