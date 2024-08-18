92295c28460ebd30c8ab64eb8fbf8825dbdbb968
jinguihua <156711203@qq.com>
2024-08-18 10:56:35 +0800

    changed

--- base.go
+++ base.go
@@ -10,8 +10,6 @@ type (
 		Top, Right, Bottom, Left float32
 	}
 
-	Position int
-
 	Page interface {
 		Content() fyne.CanvasObject
 		WinTitle() string
@@ -20,16 +18,42 @@ type (
 	}
 )
 
+// MainAxisAlignment 定义了主轴对齐方式
+type MainAxisAlignment int
+
+// MainAxisAlignment 的可能取值
 const (
-	TopLeft Position = iota
-	TopCenter
-	TopRight
-	CenterLeft
-	Center
-	CenterRight
-	BottomLeft
-	BottomCenter
-	BottomRight
+	MainAxisAlignStart        MainAxisAlignment = iota // 将子控件在主轴方向上对齐到起始位置
+	MainAxisAlignEnd                                   // 将子控件在主轴方向上对齐到结束位置
+	MainAxisAlignCenter                                // 将子控件在主轴方向上居中对齐
+	MainAxisAlignSpaceBetween                          // 在子控件之间均匀分配剩余空间
+	MainAxisAlignSpaceAround                           // 在子控件周围均匀分配剩余空间
+	MainAxisAlignSpaceEvenly                           // 在子控件之间和两端均匀分配剩余空间
+)
+
+// CrossAxisAlignment 定义了交叉轴对齐方式
+type CrossAxisAlignment int
+
+// CrossAxisAlignment 的可能取值
+const (
+	CrossAxisAlignStart   CrossAxisAlignment = iota // 子控件在交叉轴方向上对齐到起始位置
+	CrossAxisAlignEnd                               // 子控件在交叉轴方向上对齐到结束位置
+	CrossAxisAlignCenter                            // 子控件在交叉轴方向上居中对齐
+	CrossAxisAlignStretch                           // 子控件在交叉轴方向上拉伸以填充可用空间
+)
+
+type Position int
+
+const (
+	PositionTopLeft Position = iota
+	PositionTopCenter
+	PositionTopRight
+	PositionCenterLeft
+	PositionCenter
+	PositionCenterRight
+	PositionBottomLeft
+	PositionBottomCenter
+	PositionBottomRight
 )
 
 const (
@@ -52,46 +76,46 @@ func ChildPosition(position Position, parentSize, childSize fyne.Size) fyne.Posi
 	var x, y float32
 
 	switch position {
-	case TopLeft:
+	case PositionTopLeft:
 		x, y = 0, 0
 		break
 
-	case TopCenter:
+	case PositionTopCenter:
 		x = (parentSize.Width - childSize.Width) / 2
 		y = 0
 		break
 
-	case TopRight:
+	case PositionTopRight:
 		x = parentSize.Width - childSize.Width
 		y = 0
 		break
 
-	case CenterLeft:
+	case PositionCenterLeft:
 		x = 0
 		y = (parentSize.Height - childSize.Height) / 2
 		break
 
-	case Center:
+	case PositionCenter:
 		x = (parentSize.Width - childSize.Width) / 2
 		y = (parentSize.Height - childSize.Height) / 2
 		break
 
-	case CenterRight:
+	case PositionCenterRight:
 		x = parentSize.Width - childSize.Width
 		y = (parentSize.Height - childSize.Height) / 2
 		break
 
-	case BottomLeft:
+	case PositionBottomLeft:
 		x = 0
 		y = parentSize.Height - childSize.Height
 		break
 
-	case BottomCenter:
+	case PositionBottomCenter:
 		x = (parentSize.Width - childSize.Width) / 2
 		y = parentSize.Height - childSize.Height
 		break
 
-	case BottomRight:
+	case PositionBottomRight:
 		x = parentSize.Width - childSize.Width
 		y = parentSize.Height - childSize.Height
 		break
