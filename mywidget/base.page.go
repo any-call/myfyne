5808236471494e38e9fd40399f3d4395c9fe9ab7
jinguihua <156711203@qq.com>
2024-09-06 14:40:43 +0800

    changed

---
+++ mywidget/base.page.go
@@ -0,0 +1,34 @@
+package mywidget
+
+import "fyne.io/fyne/v2"
+
+type BasePage struct {
+	title string
+	id    int
+}
+
+func (self *BasePage) Content() fyne.CanvasObject {
+	panic("Content() method must be implemented by the concrete page")
+}
+
+func (self *BasePage) WinTitle() string {
+	return self.title
+}
+
+func (self *BasePage) WinID() int {
+	return self.id
+}
+
+func (self *BasePage) WinSize() fyne.Size {
+	panic("WinSize() method must be implemented by the concrete page")
+}
+
+func (self *BasePage) SetWinID(id int) {
+	self.id = id
+	return
+}
+
+func (self *BasePage) SetTitle(title string) {
+	self.title = title
+	return
+}
