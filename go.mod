29c4996c0938a81415c076ecdc06fc93cdff3c53
jinguihua <156711203@qq.com>
2024-09-25 08:30:31 +0800

    changed

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	fyne.io/x/fyne v0.0.0-20240803204126-8b5b5bfe65ef // indirect
 	github.com/fredbi/uri v1.1.0 // indirect
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
@@ -16,6 +17,7 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jeandeaual/go-locale v0.0.0-20240223122105-ce5225dcaa49 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
 	github.com/rymdport/portal v0.2.2 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
--- mywidget/date.picker.go
+++ mywidget/date.picker.go
@@ -1,141 +1,79 @@
 package mywidget
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"strconv"
+	xwidget "fyne.io/x/fyne/widget"
 	"time"
 )
 
+// DatePicker 继承 widget.BaseWidget，定义日期选择器组件
 type DatePicker struct {
 	widget.BaseWidget
-	yearSel  *widget.Select
-	monthSel *widget.Select
-	daySel   *widget.Select
-	current  time.Time
+	selectedDate  time.Time
+	calendarPop   *widget.PopUp
+	displayButton *widget.Button
+	onChanged     func(t time.Time)
 }
 
-// NewDatePicker 创建日期选择器，如果未设置日期，则默认显示当前日期
-func NewDatePicker() *DatePicker {
-	picker := &DatePicker{
-		current: time.Now(), // 设置当前日期为默认值
+// NewDatePicker 创建一个新的 DatePicker 控件
+func NewDatePicker(t time.Time, changed func(t time.Time)) *DatePicker {
+	dp := &DatePicker{
+		onChanged:    changed,
+		selectedDate: t,
 	}
 
-	// 初始化年份、月份、日期选择
-	picker.initSelectors()
+	dp.displayButton = widget.NewButton("", func() {
+		dp.showCalendar(fyne.CurrentApp().Driver().AllWindows()[0])
+	})
+	dp.displayButton.SetIcon(theme.Icon(theme.IconNameArrowDropDown))
+	dp.displayButton.IconPlacement = widget.ButtonIconTrailingText
 
-	// 设置默认日期显示
-	picker.SetDate(picker.current)
-
-	// 初始化控件
-	picker.ExtendBaseWidget(picker)
-	return picker
-}
-
-// 初始化选择框
-func (d *DatePicker) initSelectors() {
-	// 年份选择
-	years := []string{}
-	for i := 1970; i <= d.current.Year(); i++ {
-		years = append(years, strconv.Itoa(i))
-	}
-	d.yearSel = widget.NewSelect(years, nil)
-
-	// 月份选择
-	months := []string{}
-	for i := 1; i <= 12; i++ {
-		months = append(months, strconv.Itoa(i))
-	}
-	d.monthSel = widget.NewSelect(months, nil)
-
-	// 日期选择，根据当前月份确定最大天数
-	days := d.getDaysInMonth(d.current.Year(), int(d.current.Month()))
-	d.daySel = widget.NewSelect(days, nil)
-
-	// 年、月、日选择事件，动态更新日期
-	d.yearSel.OnChanged = func(_ string) {
-		d.updateDaysInMonth()
-	}
-	d.monthSel.OnChanged = func(_ string) {
-		d.updateDaysInMonth()
-	}
-}
-
-// 获取指定年份和月份的天数
-func (d *DatePicker) getDaysInMonth(year, month int) []string {
-	if month < 1 || month > 12 {
-		return []string{}
-	}
-
-	// 计算下个月的第一天
-	nextMonth := time.Month(month)%12 + 1
-	nextYear := year
-	if nextMonth == time.January {
-		nextYear++
-	}
-
-	// 使用 time.Date 计算当月的最后一天
-	firstDayOfNextMonth := time.Date(nextYear, nextMonth, 1, 0, 0, 0, 0, time.UTC)
-	lastDayOfMonth := firstDayOfNextMonth.AddDate(0, 0, -1)
-
-	// 获取当月的天数
-	totalDays := lastDayOfMonth.Day()
-	// 创建包含当月所有天数的切片
-	days := make([]string, totalDays)
-	for i := 1; i <= totalDays; i++ {
-		days[i-1] = fmt.Sprintf("%d", i)
-	}
-
-	return days
-}
-
-// 根据选择的年份和月份更新日期选择框
-func (d *DatePicker) updateDaysInMonth() {
-	year, _ := strconv.Atoi(d.yearSel.Selected)
-	month, _ := strconv.Atoi(d.monthSel.Selected)
-	if year == 0 || month == 0 {
-		return
-	}
-
-	days := d.getDaysInMonth(year, month)
-	selectedDay := d.daySel.Selected
-
-	// 重新设置日期选择
-	d.daySel.Options = days
-
-	// 保留当前选择的日期，如果超出范围，则自动选最后一天
-	tmpSelectDay, _ := strconv.Atoi(selectedDay)
-	if selectedDay == "" || tmpSelectDay > len(days) {
-		d.daySel.SetSelected(days[len(days)-1])
+	if dp.selectedDate.IsZero() {
+		dp.displayButton.SetText("请选择日期")
 	} else {
-		d.daySel.SetSelected(selectedDay)
+		dp.displayButton.SetText(dp.selectedDate.Format("2006-01-02"))
 	}
+
+	dp.ExtendBaseWidget(dp)
+	return dp
 }
 
-// SetDate 设置选择器的日期
-func (d *DatePicker) SetDate(t time.Time) {
-	d.yearSel.SetSelected(strconv.Itoa(t.Year()))
-	d.monthSel.SetSelected(strconv.Itoa(int(t.Month())))
-	d.daySel.SetSelected(strconv.Itoa(t.Day()))
+// CreateRenderer 实现自定义渲染器
+func (dp *DatePicker) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(dp.displayButton)
 }
 
-// GetDate 获取当前选择的日期
-func (d *DatePicker) GetDate() time.Time {
-	year, _ := strconv.Atoi(d.yearSel.Selected)
-	month, _ := strconv.Atoi(d.monthSel.Selected)
-	day, _ := strconv.Atoi(d.daySel.Selected)
-
-	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+func (dp *DatePicker) GetDate() time.Time {
+	return dp.selectedDate
 }
 
-// CreateRenderer 实现 fyne.WidgetRenderer 接口
-func (d *DatePicker) CreateRenderer() fyne.WidgetRenderer {
-	// 将选择框组件排列成水平容器
-	content := container.NewHBox(NewWidthBox(75, d.yearSel), canvas.NewText("年", nil),
-		NewWidthBox(50, d.monthSel), canvas.NewText("月", nil),
-		NewWidthBox(58, d.daySel), canvas.NewText("日", nil))
-	return widget.NewSimpleRenderer(content)
+// showCalendar 弹出日历选择框，显示在 DatePicker 的下方
+func (dp *DatePicker) showCalendar(w fyne.Window) {
+	calendar := xwidget.NewCalendar(dp.selectedDate, func(t time.Time) {
+		oldTime := dp.selectedDate
+		dp.selectedDate = t
+		dp.displayButton.SetText(dp.selectedDate.Format("2006-01-02"))
+		dp.calendarPop.Hide()
+		if dp.onChanged != nil && !oldTime.Equal(t) {
+			dp.onChanged(t)
+		}
+	})
+
+	// 获取 DatePicker 的相对位置
+	pos := dp.popupPos()
+
+	// 使用弹窗显示 Calendar，显示在 DatePicker 的下方
+	dp.calendarPop = widget.NewPopUp(container.NewVBox(calendar), w.Canvas())
+
+	// 显示日历的弹窗，设置位置在 DatePicker 下方
+	//dp.calendarPop.ShowAtPosition(fyne.NewPos(pos.X, pos.Y+dp.Size().Height))
+	dp.calendarPop.ShowAtPosition(pos)
+}
+
+func (db *DatePicker) popupPos() fyne.Position {
+	buttonPos := fyne.CurrentApp().Driver().AbsolutePositionForObject(db)
+	return buttonPos.Add(fyne.NewPos(0, db.Size().Height-db.Theme().Size(theme.SizeNameInputBorder)))
 }
