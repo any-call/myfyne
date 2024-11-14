aeb8926015427eae17cb9da408eab73b22081eb3
jinguihua <156711203@qq.com>
2024-11-14 14:45:24 +0800

    changed

--- mywidget/tapContainer.go
+++ mywidget/tapContainer.go
@@ -8,23 +8,27 @@ import (
 // ClickableContainer 是一个自定义容器控件，支持单击和双击事件。
 type TapContainer struct {
 	widget.BaseWidget
-	Child    fyne.CanvasObject // 容器内的子控件
-	OnTapped func()            // 单击回调
+	child    fyne.CanvasObject // 容器内的子控件
+	onTapped func()            // 单击回调
 }
 
 func NewTapContainer(child fyne.CanvasObject, onTap func()) *TapContainer {
 	c := &TapContainer{
-		Child:    child,
-		OnTapped: onTap,
+		child:    child,
+		onTapped: onTap,
 	}
 	c.ExtendBaseWidget(c)
 	return c
 }
 
+func (c *TapContainer) SetOnTap(onFn func()) {
+	c.onTapped = onFn
+}
+
 // Tapped 处理单击和双击事件。
 func (c *TapContainer) Tapped(ev *fyne.PointEvent) {
-	if c.OnTapped != nil {
-		c.OnTapped()
+	if c.onTapped != nil {
+		c.onTapped()
 	}
 }
 
@@ -33,5 +37,5 @@ func (c *TapContainer) TappedSecondary(_ *fyne.PointEvent) {}
 
 // CreateRenderer 创建容器的渲染器。
 func (c *TapContainer) CreateRenderer() fyne.WidgetRenderer {
-	return widget.NewSimpleRenderer(c.Child)
+	return widget.NewSimpleRenderer(c.child)
 }
