7a91870450a619b94c978e4664293773ac887373
jinguihua <156711203@qq.com>
2025-06-04 15:57:57 +0800

    chagned

---
+++ base.save.go
@@ -0,0 +1,32 @@
+package myfyne
+
+import (
+	"encoding/json"
+	"fyne.io/fyne/v2"
+	"os"
+	"path"
+)
+
+func GetLocFile(app fyne.App, fileName string) string {
+	storeLoc := app.Storage().RootURI()
+	return path.Join(storeLoc.Path(), fileName)
+}
+
+func LoadFromLocFile[T any](app fyne.App, fileName string) (*T, error) {
+	data, err := os.ReadFile(GetLocFile(app, fileName))
+	if err != nil {
+		return nil, err
+	}
+	var creds T
+	err = json.Unmarshal(data, &creds)
+	return &creds, err
+}
+
+func SaveToLocFile(app fyne.App, fileName string, obj any) error {
+	// 示例中未加密，生产环境应加密 password
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(GetLocFile(app, fileName), data, 0600)
+}
