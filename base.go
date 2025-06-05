d96bba848ed7fe321a5aaa3d9ebe310d2ee1cdb0
jinguihua <156711203@qq.com>
2025-06-05 19:26:27 +0800

    chagned

--- base.go
+++ base.go
@@ -161,3 +161,17 @@ func containsObject(root, obj fyne.CanvasObject) bool {
 	}
 	return false
 }
+
+// RunOnMain 提交一个 UI 操作到主线程执行（同步阻塞）
+func RunOnMain(fn func()) {
+	if fn != nil {
+		fyne.DoAndWait(fn)
+	}
+}
+
+// RunOnMainAsync 提交一个 UI 操作到主线程执行（异步不阻塞）
+func RunOnMainAsync(fn func()) {
+	if fn != nil {
+		fyne.Do(fn)
+	}
+}
