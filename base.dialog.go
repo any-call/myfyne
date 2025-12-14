59525021ac48cdfe316566789b8da7d1cebaef61
jinguihua <156711203@qq.com>
2025-12-14 18:13:04 +0800

    changed

--- base.dialog.go
+++ base.dialog.go
@@ -1,16 +1,32 @@
 package myfyne
 
 import (
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
-	"time"
 )
 
 func ShowDialogWithCallback(win fyne.Window, content DialogContent, onClose func(param any)) dialog.Dialog {
 	// 通过 content 的 Content() 方法获取展示的 fyne.CanvasObject
 	dlg := dialog.NewCustomWithoutButtons(content.Title(), content.Content(), win)
+	// 设置 content 的 dialog 对象
+	content.SetDialog(dlg)
+	content.SetWindow(win)
+	dlg.SetOnClosed(func() {
+		if onClose != nil {
+			onClose(content.GetCloseParam()) // 通过回调传递关闭时的参数
+		}
+	})
+	dlg.Show() // 显示 dialog
+	return dlg
+}
+
+func ShowDialogBySize(win fyne.Window, size fyne.Size, content DialogContent, onClose func(param any)) dialog.Dialog {
+	// 通过 content 的 Content() 方法获取展示的 fyne.CanvasObject
+	dlg := dialog.NewCustomWithoutButtons(content.Title(), container.NewGridWrap(size, content.Content()), win)
 	// 设置 content 的 dialog 对象
 	content.SetDialog(dlg)
 	content.SetWindow(win)
